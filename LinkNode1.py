56c002193f7ca7db51858eb2ec8eae0ebc3edee9
Yiyang <dyy401453043@163.com>
2021-05-20 15:20:41 +0800

    牛客网NC78 反转链表 赋值特性解法

--- LinkNode1.py
+++ LinkNode1.py
@@ -6,18 +6,27 @@
 #         self.next = None
 class Solution:
     # 返回ListNode
+    # def ReverseList(self, pHead):
+    #     stack = []
+    #     if not pHead:
+    #         return None
+    #     while pHead:
+    #         stack.append(pHead)
+    #         pHead = pHead.next
+    #     new_head = stack.pop()
+    #     result = new_head
+    #     while(len(stack) > 0):
+    #         temp = stack.pop()
+    #         new_head.next = temp
+    #         new_head = temp
+    #     new_head.next = None
+    #     return result
+
+    # 赋值特性解法,牛
     def ReverseList(self, pHead):
-        stack = []
         if not pHead:
             return None
+        pre = None
         while pHead:
-            stack.append(pHead)
-            pHead = pHead.next
-        new_head = stack.pop()
-        result = new_head
-        while(len(stack) > 0):
-            temp = stack.pop()
-            new_head.next = temp
-            new_head = temp
-        new_head.next = None
-        return result
+            pHead.next, pre, pHead= pre, pHead, pHead.next
+        return pre
