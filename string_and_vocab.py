f82de5f21c3768e66236928e79fb38cc5a19d09f
Yiyang <dyy401453043@163.com>
2021-06-07 21:43:40 +0800

    #leetcode 139，检测字符串能否根据词表完美分割，dp，复杂度O(n2 or n*m)

--- string_and_vocab.py
+++ string_and_vocab.py
@@ -1,4 +1,4 @@
-#leetcode 139，检测字符串能否根据词表完美分割，dp，复杂度O(n2)
+#leetcode 139，检测字符串能否根据词表完美分割，dp，复杂度O(n2),两种方法
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         max_word_lenghth = max([len(i) for i in wordDict])
@@ -13,3 +13,26 @@ class Solution:
                 else:
                     dp[i] = dp[i] or (dp[j] and s[j+1:i+1] in wordDict)
         return dp[-1]
+
+def check_string_in_vocab(string, vocab):
+    length = len(string)
+    dp = [False] * length # 索引是下标，0表示第一个字母
+    for i in range(length):
+        for word in vocab:
+            if i-len(word) >= 0:
+                if dp[i-len(word)] and string[i-len(word)+1:i+1] == word:
+                    dp[i] = True
+                    break
+            elif i-len(word) == -1:
+                if string[i-len(word)+1:i+1] == word:
+                    dp[i] = True
+                    break
+            else:
+                pass
+    print(dp)
+    return dp[-1]
+
+if __name__ == '__main__':
+    string = 'two minds is better than one'
+    vocab = {' ','s','two','mind','is','better','than','one'}
+    print(check_string_in_vocab(string,vocab))
