4af739c4023d4e962a9b8ba47b87e2da9e99a7fd
Yiyang <39038834+dyy401453043@users.noreply.github.com>
2022-09-08 21:09:21 +0800

    # leetcode 316 单调栈解决移除元素后最大的问题
    # leetcode 316 单调栈解决移除元素后最大的问题

--- remove_letters.py
+++ remove_letters.py
@@ -4,7 +4,7 @@ class Solution:
         stack = []
         for i in range(len(s)):
             ch = s[i]
-            if ch in stack:
+            if ch in stack: # 不能有相同的，把相同的之间的栈元素移除不会有贡献
                 continue
             while len(stack) > 0 and ch < stack[-1]:
                 if stack[-1] in s[i+1:]:
